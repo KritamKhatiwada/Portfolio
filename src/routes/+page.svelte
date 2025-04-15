ca698f0b698f62b47406667e2f27c57c418cbb2f
kritam_al <thekritamkhatiwada@gmail.com>
2025-04-15 14:31:07 +0545

    made UI better

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,55 +9,59 @@
 	import handPic from '$lib/images/Hand-semi.png';
 	import megif from '$lib/images/Working_illustration2.gif';
 	import nameLogo from '$lib/images/name-logo.png';
-	// import maillogo from '$lib/images/mail-svgrepo-com.svg';
+	import maillogo from '$lib/images/mail-svgrepo-com.svg';
+	import linkedinlogo from '$lib/images/linkedin-rounded-svgrepo-com.svg';
 	import figma from '$lib/images/figma.svg'
-	import img4now from '$lib/images/imagefornow.jpg'
+	import instalogo from '$lib/images/instagram-svgrepo-com.svg';
+	import youtubelogo from '$lib/images/youtube-168-svgrepo-com.svg';
+	import githublogo from '$lib/images/github-square-svgrepo-com.svg';
 </script>
 
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="home" />
 </svelte:head>
-<div id="hero" class="flex  justify-between">
-	<div class="w-[30%] ">
-		<!-- <p class="text-3xl font-bold height"> Kritam Khatiwada</p> -->
-		<p class="text-lg text-center mb-6">I'm Kritam, a frontend developer and a graphic desiginer form Nepal.</p>	
-		<div ><img  class="h-90" src={megif} alt="Working Illustration"></div>
-		<div class="text-xl ">
-					</div>	
-		<div class="flex">
-		</div>
+<div id="hero" class="flex  gap-12 justify-between">
+	<div class="w-[50%]  flex gap-12 flex-col ">
+			<div class="flex flex-row  shadow-sm  items-center w-full ">
+				<div class="h-50 w-full flex border-r-1 "><img  class="h-full p-1 pr-6  " src={megif} alt="Working Illustration"></div>
+				<div class="flex flex-col h-50 w-full p-2 pl-6   bg-[#8e723d1b] justify-between ">
+					<div>
+						<p class="font-bold text-xl">Kritam Khatiwada</p>
+						
+						<p>Full-Stack Developer</p>
+						<p>UI/UX Designer</p>
+						<p class="italic">from Morang, Nepal</p>
+					</div>
+					<div class="flex gap-3 items-center justify-end">
+						<!-- <a href="https://instagram.com/Losxsus"><img src={maillogo} class="h-9" alt="Gmail link"></a> -->
+						<!-- <a href="https://youtube.com/@losx-69"><img src={youtubelogo} class="h-8" alt="youtube link"></a> -->
+						<a href="https://linked.com/KritamKhatiwada"><img src={linkedinlogo} class="h-6" alt="Linkedin link"></a>
+						<a href="https://github.com/KritamKhatiwada"><img src={githublogo} class="h-7" alt="Github link"></a>
+					</div>
+				</div>
+
+			</div>
+			<div>
+				<div class="flex-row flex flex-wrap items-center gap-x-3  gap-y-3 text-sm">
+							<button class=" p-1  bg-[#D3BD95]  border-l-6 border-[#D23939] shadow-sm"><div class="p-1 flex flex-row custom-dashed-border items-center  justify-center gap-2"><img src={sveltelogo} alt="svelte" class="h-4 ml-2 " /><div class="mr-2">Svelte</div></div></button>
+							<button class=" p-1  bg-[#D3BD95] shadow-sm">	<div class="p-1 flex flex-row custom-dashed-border items-center justify-center gap-2"><img src={jslogo} alt="Javascript" class="h-4 ml-2" /><div class="mr-2">Javascript</div></div></button>
+							<button class=" p-1  bg-[#D3BD95] shadow-sm"><div class="p-1 flex flex-row custom-dashed-border items-center justify-center gap-2"><img src={tslogo} alt="Typescript" class="h-4 ml-2 " /><div class="mr-2">TypeScript</div></div></button>
+							<button class=" p-1  bg-[#D3BD95] shadow-sm"><div class="p-1 flex flex-row custom-dashed-border items-center justify-center gap-2"><img src={gitlogo} alt="Git" class="h-5 ml-2" /><div class="mr-2">Git</div></div></button>
+							<button class=" p-1  bg-[#D3BD95] shadow-sm"><div class="p-1 flex flex-row custom-dashed-border items-center justify-center gap-2"><img src={illustratorlogo} alt="adobe illustrator" class="h-4 ml-2 " /><div class="mr-2">Illustrator</div></div></button>
+							<button class=" p-1 bg-[#D3BD95]  border-solid shadow-sm "><div class="p-1 flex flex-row custom-dashed-border items-center  justify-center gap-2"><img src={figma} alt="figma" class="h-4 ml-2" /><div class="mr-2">Figma</div></div></button>
+							<button class=" p-1  bg-[#D3BD95] shadow-sm">	<div class="p-1 flex flex-row custom-dashed-border items-center justify-center gap-2"><img src={fresco} alt="fresco" class="h-4 ml-2" /><div class="mr-2">Fresco</div></div></button>
+				</div>
+			</div>
 		
 	</div>
-	<div class=" ml-6 flex flex-col-reverse w-[60%] gap-12 ">
-		<div class="flex-row flex flex-wrap items-center gap-x-6  gap-y-6">
-			<button class=" p-2  bg-[#D3BD95]  border-l-6 border-[#D23939] shadow-md"><div class="p-2 flex flex-row custom-dashed-border items-center  justify-center gap-3"><img src={sveltelogo} alt="svelte" class="h-5 ml-2 " /><div class="mr-2">Svelte</div></div></button>
-			<button class=" p-2  bg-[#D3BD95] shadow-md">	<div class="p-2 flex flex-row custom-dashed-border items-center justify-center gap-3"><img src={jslogo} alt="Javascript" class="h-5 ml-2" /><div class="mr-2">Javascript</div></div></button>
-			<button class=" p-2  bg-[#D3BD95] shadow-md"><div class="p-2 flex flex-row custom-dashed-border items-center justify-center gap-3"><img src={tslogo} alt="Typescript" class="h-5 ml-2 " /><div class="mr-2">TypeScript</div></div></button>
-			<!-- <button class=" p-2  bg-[#D3BD95] shadow-md"><div class="p-2 flex flex-row custom-dashed-border items-center justify-center gap-3"><img src={gitlogo} alt="Git" class="h-5 ml-2" /><div class="mr-2">Git</div></div></button> -->
-			<button class=" p-2 bg-[#D3BD95]  border-solid shadow-md "><div class="p-2 flex flex-row custom-dashed-border items-center  justify-center gap-3"><img src={figma} alt="figma" class="h-6 ml-2" /><div class="mr-2">Figma</div></div></button>
-			<button class=" p-2  bg-[#D3BD95] shadow-md"><div class="p-2 flex flex-row custom-dashed-border items-center justify-center gap-3"><img src={illustratorlogo} alt="adobe illustrator" class="h-5 ml-2 " /><div class="mr-2">Illustrator</div></div></button>
-			<!-- <button class=" p-2  bg-[#D3BD95] shadow-md">	<div class="p-2 flex flex-row custom-dashed-border items-center justify-center gap-3"><img src={fresco} alt="fresco" class="h-5 ml-2" /><div class="mr-2">Fresco</div></div></button> -->
-			<!-- <button class=" p-2  bg-[#D3BD95] shadow-md">	<div class="p-2 flex flex-row custom-dashed-border items-center justify-center gap-3"><img src={illustratorlogo} alt="fresco" class="h-5 ml-2" /><div class="mr-2">Photoshop</div></div></button> -->
-			<!-- <button class=" p-2  bg-[#D3BD95] shadow-md">	<div class="p-2 flex flex-row custom-dashed-border items-center justify-center gap-3"><img src={illustratorlogo} alt="fresco" class="h-5 ml-2" /><div class="mr-2"></div></div></button> -->
+	<div class=" w-full">
+		<div class="h-120 flex flex-row custom-dashed-border ">
 		</div>
-		<div class="h-80 flex flex-row custom-dashed-border ">
-			<!-- <img src={img4now} alt="portrait" srcset="" class=" w-full h-80"> -->
-		</div>
-		
-		<!--
-			use different font 
-			fix typography (line height and space)
-			add github and email link
-			take own picture wide shot
-			add second page
-			change the figma logo to red -->
 	</div>
 </div>
+
 <div id="projects" class="flex mt-32 px-0">
-	<div class="w-[70%]">
-		<img class="" src={handPic} alt="Project Showcase" srcset="">
-	</div>
 
 	<div class="flex flex-col  w-[30%]">
 		<span class=" border-black font-bold text-7xl mb-3">PROJECTS</span>
